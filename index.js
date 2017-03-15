57b4ea08ff267dd318ec28eb98d3fa7fab8e6d3d
Luis Burgos <luis.burgos.1995@gmail.com>
2017-03-15 00:59:29 -0600

    Add verify token

--- index.js
+++ index.js
@@ -20,11 +20,13 @@ app.get('/', function (req, res) {
 
 // for Facebook verification
 app.get('/webhook/', function (req, res) {
-    if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+    if (req.query['hub.verify_token'] === 'NUESTRO_TOKEN_DE_VERIFICACION') {
         res.send(req.query['hub.challenge'])
     }
     res.send('Error, wrong token')
 })
+
+const token = "<PAGE_ACCESS_TOKEN>"
 
 // Spin up the server
 app.listen(app.get('port'), function() {
